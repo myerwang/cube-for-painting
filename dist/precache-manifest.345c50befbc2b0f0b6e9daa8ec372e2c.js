20346ad159c36c4986099f250fab4642623ae67a
WANG MENGYU <myerwang@qq.com>
2022-02-09 18:41:55 +0900

    351615

---
+++ dist/precache-manifest.345c50befbc2b0f0b6e9daa8ec372e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '79656c853d9cd64cd572',
+    url: '/css/app.b4c269c2.css',
+  },
+  {
+    revision: '7628c64f919b5733215d15fcb6922f0b',
+    url: '/index.html',
+  },
+  {
+    revision: '79656c853d9cd64cd572',
+    url: '/js/app.9edb40a8.js',
+  },
+  {
+    revision: 'f559f8470ad9fd6831fc',
+    url: '/js/chunk-vendors.c8d5e289.js',
+  },
+  {
+    revision: '7decd918c5479a7eb44c58f75a9d274d',
+    url: '/manifest.json',
+  },
+  {
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: '/robots.txt',
+  },
+]);
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,76 @@
 <template>
-  <div>
-   dadfadsf
-  </div>
+  <div id="container"></div>
 </template>
 
 <script>
+import * as THREE from 'three';
+
+const OrbitControls = require('three-orbit-controls')(THREE);
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null,
+      controls: null,
+    };
+  },
+  mounted() {
+    this.init();
+    this.animate();
+  },
+  methods: {
+    // 初始化
+    init() {
+      //  创建场景对象Scene
+      this.scene = new THREE.Scene();
+
+      // 网格模型添加到场景中
+      const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+      const material = new THREE.MeshNormalMaterial({
+        color: 'white',
+      });
+      this.mesh = new THREE.Mesh(geometry, material);
+      this.scene.add(this.mesh);
+
+      /**
+       * 相机设置
+       */
+      const container = document.getElementById('container');
+      this.camera = new THREE.PerspectiveCamera(
+        70,
+        container.clientWidth / container.clientHeight,
+        0.01,
+        10,
+      );
+      this.camera.position.z = 1;
+
+      /**
+       * 创建渲染器对象
+       */
+      this.renderer = new THREE.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
+
+      // 创建控件对象
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    },
+
+    // 动画
+    animate() {
+      requestAnimationFrame(this.animate);
+      this.renderer.render(this.scene, this.camera);
+    },
   },
 };
 </script>
 
-<style lang="less">
+<style>
+#container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
 </style>
